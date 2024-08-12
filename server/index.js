94fedebf3ee80465f9715f7058bd4ffb75a8c6be
Manish kumar <manishmandal9734@gmail.com>
2024-08-12 22:19:47 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:3000',
+    origin:'*',
     credentials:true
 };
 app.use(cors(corsOption)); 
